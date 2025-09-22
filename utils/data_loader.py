09e6933792b9323895e2cdb034b18d234090cdec
Fabrizio <fabriziodisciorio91@gmail.com>
2025-09-22 10:35:02 +0200

    Update data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,11 +2,6 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-def load_excel(file):
-    df = pd.read_excel(file)
-    df["Data fine"] = pd.to_datetime(df["Data fine"])
-    return df
-
 def load_google_sheet(json_keyfile, sheet_name):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
@@ -14,5 +9,10 @@ def load_google_sheet(json_keyfile, sheet_name):
     sheet = client.open(sheet_name).sheet1
     data = sheet.get_all_records()
     df = pd.DataFrame(data)
+    df["Data fine"] = pd.to_datetime(df["Data fine"])
+    return df
+
+def load_excel(file):
+    df = pd.read_excel(file, engine="openpyxl")
     df["Data fine"] = pd.to_datetime(df["Data fine"])
     return df
